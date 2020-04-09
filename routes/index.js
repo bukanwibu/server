3ffa54af0751cf76f4b08f468f54221379e94060
devitasari <devitas700@gmail.com>
2020-04-09 18:50:35 +0700

    register done

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
+const userRouter = require('../routes/userRouter')
 
 router.get('/', function(req, res, next) {
 	console.log('masuk')
@@ -8,5 +9,8 @@ router.get('/', function(req, res, next) {
 		message: 'You are connected to server!'
 	})
 })
+
+router.use('/users', userRouter)
+
 
 module.exports = router
