1796f88f60c65c43f03a8378cfd9fc96d9e17230
Nico Audy <nicoaudy1994@gmail.com>
2020-04-10 21:26:39 +0700

    icip2 mongoose

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,47 +1,56 @@
-const User = require('../models/user')
-const { hashPassword, checkPassword } = require('../helpers/bcrypt')
-const { generateToken, verifyToken } = require('../helpers/jwt')
+const User = require("../models/user");
+const { hashPassword, checkPassword } = require("../helpers/bcrypt");
+const { generateToken, verifyToken } = require("../helpers/jwt");
 
 class UserController {
-	
 	static register(req, res, next) {
 		User.create({
 			name: req.body.name,
 			email: req.body.email,
 			password: req.body.password
 		})
-		.then((newUser) => {
-			res.status(201).json({
-				newUser
+			.then(newUser => {
+				res.status(201).json({
+					newUser
+				});
 			})
-		})
-		.catch(next)
+			.catch(next);
 	}
 
 	static login(req, res, next) {
-		if (!req.body.identity) throw { message: "identity is required" }
-		if(!req.body.password) throw { message: "password is required" }
+		if (!req.body.identity) throw { message: "identity is required" };
+		if (!req.body.password) throw { message: "password is required" };
 
 		User.findOne({
-			$or: [{ name: req.body.identity }, { email: req.body.identity}]
+			$or: [{ name: req.body.identity }, { email: req.body.identity }]
 		})
-		.then(user => {
-			if (!user) throw { message: "invalid identity or password"}
-			let passwordInput = req.body.password
-			let passwordDb = user.password
-			let isPassword = checkPassword(passwordInput, passwordDb)
-			console.log('pas')
-			if (!isPassword) throw { message: 'invalid identity or password'}
-			let payload = {
-				id: user._id,
-				name: user.name,
-				email: user.email
-			}
-			let token = generateToken(payload)
-			res.status(200).json({token, user})
-		})
-		.catch(next)
+			.then(user => {
+				if (!user) throw { message: "invalid identity or password" };
+				let passwordInput = req.body.password;
+				let passwordDb = user.password;
+				let isPassword = checkPassword(passwordInput, passwordDb);
+				console.log("pas");
+				if (!isPassword) throw { message: "invalid identity or password" };
+				let payload = {
+					id: user._id,
+					name: user.name,
+					email: user.email
+				};
+				let token = generateToken(payload);
+				res.status(200).json({ token, user });
+			})
+			.catch(next);
+	}
+
+	static forgotPassword(req, res, next) {
+		if (!req.body.email) throw { message: "email is required" };
+		User.findOne({ email: req.body.email }).then(user => {
+			if (!user) throw { message: "invalid email" };
+
+			// Update password DB & send random password to email
+			res.status(200).json({ user });
+		});
 	}
 }
 
-module.exports = UserController
+module.exports = UserController;
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,7 +1,8 @@
-const router = require('express').Router()
-const UserController = require('../controllers/userController')
+const router = require("express").Router();
+const UserController = require("../controllers/userController");
 
-router.post('/register', UserController.register)
-router.post('/login', UserController.login)
+router.post("/register", UserController.register);
+router.post("/login", UserController.login);
+router.post("/forgot-password", UserController.forgotPassword);
 
-module.exports = router
+module.exports = router;
